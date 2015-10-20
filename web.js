96d1ef89a11a052ec859e99b19a7b4e88818477c
Nicola DiPalma <nickyd242@gmail.com>
2015-10-19 20:31:31 -0700

    Removed components intended for serving with node on Heroku.

--- web.js
+++
@@ -1,9 +0,0 @@
-// Referenced from: http://www.sitepoint.com/deploying-yeomanangular-app-heroku/ as accessed 10/19/2015
-
-var gzippo = require('gzippo');
-var express = require('express');
-var app = express();
-
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
