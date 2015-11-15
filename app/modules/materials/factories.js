df7ade56294fbf58ffd0a1655b8f49c83ec75862
Nicola DiPalma <nickyd242@gmail.com>
2015-11-15 00:31:18 -0800

    Moved client application sources back from external folder to '/app'

---
+++ app/modules/materials/factories.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+
+angular
+  .module('materials')
+  .factory('MaterialsService', [ '$http', function ($http) {
+    return $http.get('model/materials.json');
+  }]);
---
+++ app/modules/settings/factories.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+angular
+  .module('settings')
+	.factory('SettingsService', function () {
+
+    var factory = {};
+
+    factory.Tolerance = 0.1;
+
+    return factory;
+	});
