70eb6e02525a02e2400668a60d11a3c8430e1fe6
Nicola DiPalma <nickyd242@gmail.com>
2015-11-15 10:27:53 -0800

    Created templates for application metadata and styles.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET application home page. */
 router.get('/', function(req, res, next) {
-	res.render('index');
+	res.render('main');
 });
 
 module.exports = router;
