d5cd1d5d08578006619d5e8b3ee89afd62951105
Nicola DiPalma <nickyd242@gmail.com>
2015-11-15 20:19:53 -0800

    Moved app config to app root folder.

---
+++ app/config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('layer',[]);
+angular.module('materials',[]);
+angular.module('settings',[]);
+angular.module('calculator', [ 'layer', 'materials', 'settings' ]);
+
+angular
+	.module('shockwaveCalculatorWebApp', [
+		'ngCookies',
+		'ngResource',
+		'ngSanitize',
+		'ngRoute',
+		'calculator',
+		'materials',
+		'settings'
+	])
+	.config(function ($routeProvider) {
+	$routeProvider
+		.when('/Calculator', {
+			templateUrl: 'modules/calculator/view.html',
+			controller: 'CalculatorCtrl'
+		})
+		.when('/Materials', {
+			templateUrl: 'modules/materials/view.html',
+			controller: 'MaterialsCtrl'
+		})
+		.when('/Settings', {
+			templateUrl: 'modules/settings/view.html',
+			controller: 'SettingsCtrl'
+		})
+		.otherwise({
+			redirectTo: '/Calculator'
+		});
+	});
