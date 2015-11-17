ed1ab07316a83e75904392c164df4f025a03207d
Nicola DiPalma <nickyd242@gmail.com>
2015-11-16 23:29:04 -0800

    Created 'main.js' file for module-loading; removed explicit references to library scripts.

---
+++ app/main.js
@@ -0,0 +1,57 @@
+// List of library dependencies (Url paths)
+var angularLibUrl = 'bower_components/angular/angular.min';
+var angularResourceLibUrl = 'bower_components/angular-resource/angular-resource.min';
+var angularRouteLibUrl = 'bower_components/angular-route/angular-route.min';
+var jQueryLibUrl = 'bower_components/jquery/dist/jquery.min';
+
+// Begin loading library dependencies
+requirejs.config({
+	baseUrl: '/',
+    paths: {
+		angular: [
+			angularLibUrl
+		],
+		angularResource: [
+			angularResourceLibUrl
+		],
+		angularRoute: [
+			angularRouteLibUrl
+		],
+		jQuery: [
+			jQueryLibUrl
+		]
+    },
+	shim: {
+		'angular': {
+			'exports': 'angular'
+		},
+		'angularResource': {
+			deps: ['angular']
+		},
+		'angularRoute': {
+			deps: ['angular']
+		},
+		'jQuery': {
+			'exports': 'jQuery'
+		}
+	}
+});
+
+// Begin RequireJS module-load
+requirejs(
+	[
+		'angular',
+		'angularResource',
+		'angularRoute',
+		'jQuery'
+	], function(
+			angular
+		) {
+
+		requirejs([], function(shockwaveCalculator) {
+			angular.element(document).ready(function() {
+				angular.bootstrap(document.body, ['shockwaveCalculator']);
+			});
+		});
+
+});
