b98bad291e841720abaff88584f203662e1cb7d6
Nicola DiPalma <nickyd242@gmail.com>
2015-11-22 17:51:11 -0800

    Added 'serve-favicon' to express application script.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
 // create app instance
 var app = express();
+
+// set application favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
